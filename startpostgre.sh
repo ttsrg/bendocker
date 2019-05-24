e3042f26bc3030168f80a7929fc8f7ef330454f1
Your Name <you@example.com>
2019-05-24 12:47:36 +0000

    6 docker/postgre using created volume

--- startpostgre.sh
+++ startpostgre.sh
@@ -23,11 +23,16 @@ docker build -t $CONTAINER_IMAGE:$POSTGRE_VER --build-arg POSTGRE_VER=$POSTGRE_V
 ### ports?
 
 #docker rm $CONTAINER_NAME 
-mkdir -p volumes/{gerrit, postgre}
+#mkdir -p volumes/{gerrit, postgre}
+#mkdir -p $(pwd)/volumes/postgre/{etc/,var\/log,var\/lib}
+mkdir -p  $(pwd)/var/lib/postgresql
 
 ### if net
-#docker network create gerrit-net
+docker network create gerrit-net
+docker volume create posvol
 docker rm -f $CONTAINER_NAME
 #docker run  -d --restart=always --network=gerrit-net --name=$CONTAINER_NAME  $CONTAINER_IMAGE:$POSTGRE_VER 
 #docker run  -d  --network=gerrit-net --name=$CONTAINER_NAME  $CONTAINER_IMAGE:$POSTGRE_VER 
-docker run  -d   --network=gerrit-net --name=$CONTAINER_NAME  $CONTAINER_IMAGE:$POSTGRE_VER
+docker run  -d  -v posvol:/var/lib/postgresql  --network=gerrit-net --name=$CONTAINER_NAME  $CONTAINER_IMAGE:$POSTGRE_VER
+
+#docker run  -d -v $(pwd)/volumes/postgre/etc:/etc/postgresql -v $(pwd)/volumes/postgre/var/log:/var/log/postgresql -v $(pwd)/volumes/postgre/var/lib:/var/lib/postgresql  --network=gerrit-net --name=$CONTAINER_NAME  $CONTAINER_IMAGE:$POSTGRE_VER
